662954848032d70112c80db29d0bf8e1f4e9176a
romanoBeer <beerromano@gmail.com>
2023-05-02 11:41:25 +0200

    Added search function in comments

--- script.js
+++ script.js
@@ -11,9 +11,36 @@ fetch('cars.xml')
         const carSearch = document.getElementById('car-search');
         // carSearch.addEventListener('change')
         carLoop(vehicles, carCounter)
-        
     })
     .catch(error => console.error(error));
+
+    // // ChatGPT Search
+// fetch('cars.xml')
+//     .then(response => response.text())
+//     .then(xmlString => {
+//         // Parse the XML string into an XML document
+//         const parser = new DOMParser();
+//         const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+//         const carCounter = document.getElementById('car-counter');
+//         // Get the list of vehicles from the XML document
+//         const vehicles = xmlDoc.getElementsByTagName('Vehicle');
+//         const carSearch = document.getElementById('car-search');
+//         carSearch.addEventListener('input', () => {
+//             const inputValue = carSearch.value.toLowerCase();
+//             for (let i = 0; i < vehicles.length; i++) {
+//                 const vehicle = vehicles[i];
+//                 const make = vehicle.getElementsByTagName('MakeName')[0].textContent.toLowerCase();
+//                 const gridItem = document.getElementsByClassName('grid-item')[i];
+//                 if (make.includes(inputValue)) {
+//                     gridItem.style.display = 'block';
+//                 } else {
+//                     gridItem.style.display = 'none';
+//                 }
+//             }
+//         });
+//         carLoop(vehicles, carCounter);
+//     })
+//     .catch(error => console.error(error));
 
 
 const carLoop = (vehicles, carCounter) => {
@@ -38,7 +65,7 @@ const carLoop = (vehicles, carCounter) => {
         gridItem.classList.add('grid-item');
 
         // Add the vehicle details to the grid item
-        if(typeof imageElement !== "undefined") {
+        if (typeof imageElement !== "undefined") {
             counter++;
             carCounter.innerHTML = counter + " Vehicles Listed";
             gridItem.innerHTML = `
@@ -50,16 +77,16 @@ const carLoop = (vehicles, carCounter) => {
                     <p>Price: R${price}</p>
                 </div>
             `;
-        
 
-        // Closes modal on overlay click
-        modal.addEventListener('click', () => {
-            modal.style.display = 'none';            
-        })
-        // Add a click event listener to the grid item
-        gridItem.addEventListener('click', () => {
-            // Set the modal content
-            modal.innerHTML = `
+
+            // Closes modal on overlay click
+            modal.addEventListener('click', () => {
+                modal.style.display = 'none';
+            })
+            // Add a click event listener to the grid item
+            gridItem.addEventListener('click', () => {
+                // Set the modal content
+                modal.innerHTML = `
                 <div class="modal-content">
                     <div class="modal-details">
                         <h2>${make} ${model} (${year})</h2>
@@ -74,36 +101,36 @@ const carLoop = (vehicles, carCounter) => {
                 </div>
             `;
 
-            // Show the modal
-            modal.style.display = 'block';
+                // Show the modal
+                modal.style.display = 'block';
 
-            // Get the close button element inside the modal
-            const modalCloseButton = modal.querySelector('.close');
+                // Get the close button element inside the modal
+                const modalCloseButton = modal.querySelector('.close');
 
-            // Add a click event listener to the close button
-            modalCloseButton.addEventListener('click', () => {
-                // Hide the modal
-                modal.style.display = 'none';
+                // Add a click event listener to the close button
+                modalCloseButton.addEventListener('click', () => {
+                    // Hide the modal
+                    modal.style.display = 'none';
+                });
+
+                // Calculate the position of the modal
+                const modalContent = modal.querySelector('.modal-content');
+                const modalWidth = modalContent.offsetWidth;
+                const modalHeight = modalContent.offsetHeight;
+                const screenWidth = window.innerWidth;
+                const screenHeight = window.innerHeight;
+                const left = (screenWidth - modalWidth) / 2;
+                const top = (screenHeight - modalHeight) / 2;
+
+                // Set the position of the modal
+                modalContent.style.left = left + 'px';
+                modalContent.style.top = top + 'px';
             });
 
-            // Calculate the position of the modal
-            const modalContent = modal.querySelector('.modal-content');
-            const modalWidth = modalContent.offsetWidth;
-            const modalHeight = modalContent.offsetHeight;
-            const screenWidth = window.innerWidth;
-            const screenHeight = window.innerHeight;
-            const left = (screenWidth - modalWidth) / 2;
-            const top = (screenHeight - modalHeight) / 2;
-
-            // Set the position of the modal
-            modalContent.style.left = left + 'px';
-            modalContent.style.top = top + 'px';
-        });
-
-        // Add the grid item to the grid container
-        document.getElementById('car-grid').appendChild(gridItem);
-    } else {
-        gridItem.innerHTML = "None"
-    }
+            // Add the grid item to the grid container
+            document.getElementById('car-grid').appendChild(gridItem);
+        } else {
+            gridItem.innerHTML = "None"
+        }
     }
 }
